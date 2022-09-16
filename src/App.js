da0095a28b9da9583ee0633750ac63269917e807
Sergey Dyatlov <s.dyatlov90@gmail.com>
2022-09-16 21:21:41 +0200

    hooks refactoring

--- src/App.js
+++ src/App.js
@@ -1,25 +1,145 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import shortid from "shortid";
+import { Notify } from "notiflix/build/notiflix-notify-aio";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+import Phonebook from "./Components/Phonebook";
+import AddContacts from "./Components/AddContacts";
+import Filter from "./Components/Filter";
+import s from "./Components/Phonebook.module.css";
+
+import { useState, useEffect } from "react";
+import useLocaleStorage from "./Components/localStorage";
+import contactEl from "./Components/contacts.json";
+
+// class App extends React.Component {
+//   state = {
+//     contacts: [
+//       { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
+//       { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
+//       { id: "id-3", name: "Eden Clements", number: "645-17-79" },
+//       { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
+//     ],
+//     filter: "",
+//   };
+
+//   componentDidMount() {
+//     const contacts = localStorage.getItem("contacts");
+//     const parsedContacts = JSON.parse(contacts);
+//     if (parsedContacts) {
+//       this.setState({ contacts: parsedContacts });
+//     }
+//   }
+
+//   componentDidUpdate(prevProps, prevState) {
+//     if (this.state.contacts !== prevState.contacts) {
+//       localStorage.setItem("contacts", JSON.stringify(this.state.contacts));
+//     }
+//   }
+
+//   addContact = (name, number) => {
+//     console.log(name, number);
+
+//     const { contacts } = this.state;
+//     const repeatName = contacts.find((contact) => {
+//       return contact.name.toLowerCase() === name.toLowerCase();
+//     });
+//     if (repeatName) {
+//       Notify.warning(`${name} is already in contacts`);
+//       return;
+//     }
+
+//     const contact = {
+//       id: shortid.generate(),
+//       name,
+//       number,
+//     };
+
+//     this.setState(({ contacts }) => ({
+//       contacts: [contact, ...contacts],
+//     }));
+//     Notify.success(`${name} is added in contacts`);
+//   };
+
+//   nameInputId = shortid.generate();
+
+//   deleteContact = (contactId) => {
+//     this.setState((prevState) => ({
+//       contacts: prevState.contacts.filter(
+//         (contact) => contact.id !== contactId
+//       ),
+//     }));
+//   };
+
+//   changeFilter = (e) => {
+//     this.setState({ filter: e.target.value });
+//   };
+
+//   visibleContactCards = () => {
+//     const { filter, contacts } = this.state;
+//     const normalizedFilter = filter.toLowerCase();
+
+//     return contacts.filter((contact) =>
+//       contact.name.toLowerCase().includes(normalizedFilter)
+//     );
+//   };
+
+//   render() {
+//     const { contacts, filter } = this.state;
+//     const visibleContactCards = this.visibleContactCards();
+
+const App = () => {
+  const [contacts, setContacts] = useLocaleStorage("contact", contactEl);
+  const [filter, setFilter] = useState("");
+
+  useEffect(() => {
+    localStorage.setItem("contact", JSON.stringify(contacts));
+  }, [contacts]);
+
+  const filterChange = (e) => {
+    setFilter(e.target.value);
+  };
+
+  const addContactCard = ({ name, number }) => {
+    const repeatName = contacts.find((contact) => {
+      return contact.name.toLowerCase() === name.toLowerCase();
+    });
+    if (repeatName) {
+      Notify.warning(`${name} is already in contacts`);
+      return;
+    }
+    const contact = {
+      id: shortid.generate(),
+      name,
+      number,
+    };
+    setContacts((prev) => {
+      return [contact, ...prev];
+    });
+
+    Notify.success(`${name} is added in contacts`);
+  };
+
+  const deleteContactCard = (cardId) => {
+    setContacts((prev) => prev.filter((contact) => contact.id !== cardId));
+  };
+
+  const normalizedFilter = filter.toLowerCase();
+  const visibleContactCards = contacts.filter((contact) =>
+    contact.name.toLowerCase().includes(normalizedFilter)
   );
-}
+
+  return (
+    <section className={s.container}>
+      <h1>Phonebook</h1>
+      <AddContacts onAddContactCard={addContactCard} />
+      <h2>Contacts</h2>
+      <Filter onChangeFilter={filterChange} value={filter} />
+      <Phonebook
+        contacts={visibleContactCards}
+        onDeleteContact={deleteContactCard}
+      />
+    </section>
+  );
+};
 
 export default App;
---
+++ src/Components/AddContacts.js
@@ -0,0 +1,85 @@
+// import { Component } from "react";
+import s from "./Phonebook.module.css";
+
+// class AddContacts extends Component {
+//   state = {
+//     name: "",
+//     number: "",
+//   };
+
+//   handleSubmit = (e) => {
+//     e.preventDefault();
+//     console.log(this.state);
+//     this.props.onSubmit(this.state.name, this.state.number);
+//     this.setState({ name: "", number: "" });
+//   };
+
+//   handleChangeName = (e) => {
+//     this.setState({ name: e.currentTarget.value });
+//   };
+//   handleChangeNumber = (e) => {
+//     this.setState({ number: e.currentTarget.value });
+//   };
+
+import { useState } from "react";
+
+const AddContacts = ({ onAddContactCard }) => {
+  const [name, setName] = useState("");
+  const [number, setNumber] = useState("");
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+
+    switch (name) {
+      case "name":
+        setName(value);
+        break;
+      case "number":
+        setNumber(value);
+        break;
+      default:
+        return;
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onAddContactCard({ name, number });
+    setNumber("");
+    setName("");
+  };
+
+  return (
+    <form className={s.phonebookList} onSubmit={handleSubmit}>
+      <label>Name</label>
+      <input
+        className={s.input}
+        type="text"
+        name="name"
+        value={name}
+        onChange={handleChange}
+        pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+        title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+        required
+      />
+
+      <label>Number</label>
+      <input
+        className={s.input}
+        type="tel"
+        name="number"
+        value={number}
+        onChange={handleChange}
+        pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+        title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+        required
+      />
+
+      <button className={s.button} type="submit">
+        Add contact
+      </button>
+    </form>
+  );
+};
+
+export default AddContacts;
